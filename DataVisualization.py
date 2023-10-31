ff4d242f50f3fd25e2c01a158a0a4931cccda966
Anmol Devansh <102401768+theanmoldevansh@users.noreply.github.com>
2023-10-31 08:48:37 +0530

    Add files via upload

---
+++ DataVisualization.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5]
+y = [10, 16, 20, 25, 30]
+
+plt.plot(x, y)
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Simple Line Plot')
+plt.show()
